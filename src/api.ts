a82d8e7c1fb78938501cb496f716ebde0e3506a9
rwrq22 <61651734+rwrq22@users.noreply.github.com>
2024-09-30 11:49:32 +0900

    update URL

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000/api/v1/"
-      : "https://qabot-dlq3.onrender.com/api/v1/",
+      : "https://backend.qabotjs.xyz/api/v1/",
   withCredentials: true,
 });
 
